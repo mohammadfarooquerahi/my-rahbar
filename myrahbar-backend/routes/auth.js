943929bfa95593070f49f34ba87779674ca204b2
farooqrahi006 <farooqrahi828@gmail.com>
2026-07-12 15:26:45 +0500

    feat(auth): add temporary endpoint to upgrade user to admin

--- myrahbar-backend/routes/auth.js
+++ myrahbar-backend/routes/auth.js
@@ -13,4 +13,20 @@ router.post("/login", login);
 router.get("/me", protect, getMe);
 router.put("/profile", protect, updateProfile);
 
+// TEMPORARY: Upgrade any email to admin
+router.get('/make-admin/:email', async (req, res) => {
+  const User = require('../models/User');
+  try {
+    const user = await User.findOneAndUpdate(
+      { email: req.params.email }, 
+      { role: 'admin' }, 
+      { new: true }
+    );
+    if (!user) return res.status(404).send('User not found in database. Make sure you registered this email first.');
+    res.send('<h2>Success!</h2><p><b>' + user.email + '</b> is now an ADMIN.</p><p>Please go to the website, <b>Log Out</b>, and <b>Log In again</b>. Then open the Admin Panel and all data will show!</p>');
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
 module.exports = router;
